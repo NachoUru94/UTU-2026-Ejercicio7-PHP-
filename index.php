b268a7940219ee1d54219bb9f3d5604b05a214ad
NachoUru94 <124197288+NachoUru94@users.noreply.github.com>
2026-06-04 20:54:40 -0300

    commit clase dia 4 de junio del 2026 - agregando primeras variables - if - switch - for- foreach- while - array

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+ $nombre = "jose"; //String
+ $edad = 31; //Integer
+ $miDinero = 1000.50; //Float
+ $esMayorDeEdad = false; //Boolean
+ $tieneFerrari = null; //sin valor
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +16,49 @@
 </head>
 <body>
   <h1>Ejercicio 7</h1>
+    <p>C:\xampp\htdocs\UTU-2026-Ejercicio7-PHP</p>
     
+    <h2><?php echo $nombre; ?></h2>
+   
+    
+    <?php //if else if y else
+    if($edad >= 18){
+      $mayorDeEdad = true;
+    }else if($edad < 18){
+      $mayorDeEdad = false;
+    }else{
+      $edad=null;
+    }
+    
+    switch(true){
+      case $edad > 18:
+        $mayorDeEdad = true;
+        break;
+      case $edad < 18:
+        $mayorDeEdad = false;
+        break;
+      default:
+        $edad = null;
+    }
 
+    for($i = 0; $i < 3; $i++){
+      $edad++;
+    }
 
+    while($edad < 35){
+      $edad++;
+    }
 
+      //lo utilisaremos para integrar en un array elementos de diferentes tipos de datos
+      $frutas = array("manzana", "banana", "naranja");
+      $verduras = ["lechuga", "tomate", "zanahoria", 456, false, null];
+
+    foreach( $frutas as $fruta){
+      echo "<h3>" . "Mi fruta es: " . $fruta . "</h3><br>";
+    }
+    ?>
+
+  
 
   </body>
 </html>
